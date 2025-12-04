e3abcb60d772a952c117b96e9bd38e5579c3e520
ilya034 <68101303+ilya034@users.noreply.github.com>
2025-12-04 18:18:36 +0500

    Revert "Add Avalonia UI project with Graph rendering controls (WIP)"
    This reverts commit f2d5397acbd4f949d2462ec53984f00c93233ebe.

--- GraphCalcTest1.Avalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GraphCalcTest1.Avalonia.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- GraphCalcTest1.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace GraphCalcTest1.Avalonia.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
